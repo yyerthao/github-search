a2dc213f290c4ef8faaa464ca35b02fda7b4fa9b
Yer Thao <maivyerthao@gmail.com>
2021-05-30 08:35:59 -0500

    Initial input field styling and Results component set up

---
+++ src/components/Results/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Results(){
+    return(
+        <section>
+            <h2 className="grey-color">Results will render here</h2>
+        </section>
+    )
+}
+
+export default Results;
--- src/components/SearchQuery/SearchQuery.js
+++ src/components/SearchQuery/SearchQuery.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Results from '../Results/Results';
 
 function SearchQuery(){
  return(
      <>
      <section className="center">
-        <input placeholder="Enter search query keywords here"></input>
+        <input placeholder="Enter search query keywords here" className="one-rem-margin input-field"></input>
         <button>Search</button>
+        <Results/>
      </section>
      </>
  )
