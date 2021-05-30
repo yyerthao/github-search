936997a4c9d8c4bb7580b84cc1d009882852cc46
Yer Thao <maivyerthao@gmail.com>
2021-05-30 10:18:08 -0500

    Implemented react hook useState to grab user input values

--- src/components/SearchQuery/SearchQuery.js
+++ src/components/SearchQuery/SearchQuery.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Results from '../Results/Results';
 
 function SearchQuery(){
+
+ const [input, setInput] = useState('');
+
+
+const handleSubmit = event => {
+    event.preventDefault(); 
+    console.log(`User input:`, input); // console logging to ensure user input action is working
+    setInput(''); // This resets our string back to empty
+    // TO DO: dispatch user search query keyword in here
+    };
+
+
  return(
-     <>
-     <section className="center">
-        <input placeholder="Enter search query keywords here" className="one-rem-margin input-field"></input>
-        <button>Search</button>
-        <Results/>
-     </section>
-     </>
+     <form 
+        onSubmit={handleSubmit}
+        className="center">
+        <input
+            type="text" 
+            onChange={  (event) => setInput(event.target.value) }
+            value={input}
+            placeholder="Enter search query keywords here" 
+            className="one-rem-margin input-field">
+        </input>
+            <button type="submit">Search</button>
+            <Results/>
+     </form>
  )
 
 }
